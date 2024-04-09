ee4901a81e99cfd7b1aaa4d75af2a4757505d470
Madhur Mongia <madhurrajput1010@gmail.com>
2024-04-10 02:04:35 +0530

    Ownable contract

--- contracts/inheritance.sol
+++ contracts/inheritance.sol
@@ -1,36 +1,29 @@
-
 pragma solidity ^0.8.0;
 
-contract InheritanceContract {
-    address public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract InheritanceContract is Ownable {
     address public heir;
     uint256 public lastWithdrawalTimestamp;
     uint256 public constant OWNERSHIP_TRANSFER_DELAY_DAYS = 30 days;
 
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-    event HeirChanged(address indexed previousHeir, address indexed newHeir);
-    event FundsWithdrawn(address indexed recipient, uint256 amount);
+    event HeirChanged(address previousHeir, address newHeir);
+    event FundsWithdrawn(address recipient, uint256 amount);
 
     constructor(address _heir) {
         require(_heir != address(0), "Heir cannot be the zero address.");
-        owner = msg.sender;
         heir = _heir;
         lastWithdrawalTimestamp = block.timestamp;
     }
 
-    function withdraw(uint256 amount) external {
-        require(msg.sender == owner, "Only the owner can withdraw funds.");
+    function withdraw(uint256 amount) external onlyOwner {
         require(
             address(this).balance >= amount,
             "Insufficient contract balance."
         );
-
         lastWithdrawalTimestamp = block.timestamp;
-        payable(owner).transfer(amount);
-        emit FundsWithdrawn(owner, amount);
+        payable(owner()).transfer(amount);
+        emit FundsWithdrawn(owner(), amount);
     }
 
     function claimOwnership(address _newheir) external {
@@ -43,22 +36,19 @@ contract InheritanceContract {
                 lastWithdrawalTimestamp + OWNERSHIP_TRANSFER_DELAY_DAYS,
             "The owner still has control."
         );
-         require(_newheir != address(0), " new Heir cannot be the zero address.");
-
-        address previousOwner = owner;
-        owner = heir;
+        require(_newheir != address(0), "New heir cannot be the zero address.");
+        address previousHeir = heir;
         heir = _newheir;
         lastWithdrawalTimestamp = block.timestamp;
-        emit OwnershipTransferred(previousOwner, owner);
+        emit OwnershipTransferred(owner(), _newheir);
+        emit HeirChanged(previousHeir, _newheir);
     }
 
-    function setHeir(address _heir) external {
-        require(msg.sender == owner, "Only the owner can set the heir.");
+    function setHeir(address _heir) external onlyOwner {
         require(_heir != address(0), "Heir cannot be the zero address.");
-        require(_heir != owner, "Owner cannot be the heir.");
-
+        require(_heir != owner(), "Owner cannot be the heir.");
         address previousHeir = heir;
         heir = _heir;
-        emit HeirChanged(previousHeir, heir);
+        emit HeirChanged(previousHeir, _heir);
     }
 }
